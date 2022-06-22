c38c468544c98b7be4ff721ea09a1016aee00261
Russell Fraze <russell.fraze@gmail.com>
2022-06-22 14:57:05 -0700

    scroll fixed

--- app.js
+++ app.js
@@ -23,3 +23,47 @@ const navSlide = () => {
 
 navSlide();
 
+// const smootheScroll = (target,duration) => {
+//     var target = document.querySelector(target);
+//     var targetPosition = target.getBoundingClientRect().top;
+//     var startPosition = window.pageYOffset;
+//     var distance = targetPosition - startPosition;
+//     var startTime = null;
+
+//     function animation(currentTime){
+//         console.log(currentTime)
+//         if(startTime === null) startTime = currentTime;
+//         var timeElapsed = currentTime - startTime;
+//         var run = easeInOutQuad(timeElapsed,startPosition,distance,duration)
+//         window.scrollTo(0,run);
+//         if(timeElapsed < duration) requestAnimationFrame(animation);
+//     }
+ 
+//     function easeInOutQuad(t, b, c, d) {
+//         t /= d/2;
+//         if (t < 1) return c/2*t*t + b;
+//         t--;
+//         return -c/2 * (t*(t-2) - 1) + b;
+//     };
+
+//     requestAnimationFrame(animation);
+// }
+
+// var about_section = document.querySelector('.about_section');
+// var projects_section = document.querySelector('.projects_section');
+// var skills_section = document.querySelector('.skills_section');
+
+// about_section.addEventListener('click',function(){
+//     smootheScroll('#about_section', 1000);
+// });
+
+
+// projects_section.addEventListener('click',function(){
+//     smootheScroll('#projects_section', 1000);
+// });
+
+
+// skills_section.addEventListener('click',function(){
+//     smootheScroll('#skills_section', 1000);
+// });
+
