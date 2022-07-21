417d6a3fd67fd76bf80249653e35bcd50a15ef52
Russell Fraze <russell.fraze@gmail.com>
2022-07-21 10:50:30 -0700

    burger fix

--- app.js
+++ app.js
@@ -1,54 +1,8 @@
-// const navSlide = () => {
-//     const burger = document.querySelector('.burger');
-//     const nav = document.querySelector('.nav-links');
-//     const navLinks = document.querySelectorAll('.nav-links li')
-
-//     burger.addEventListener('click',()=>{
-//         //toggle the nav
-//         nav.classList.toggle('nav-active')
-
-//         navLinks.forEach((link, index)=>{
-//             if(link.style.animation) {
-//                 link.style.animation = ''
-//             } else {
-//                 link.style.animation = link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 + 0.5}s`;
-//             }        
-//         })
-
-//         burger.classList.toggle('toggle');
-//     })
-
-
-
-// }
-
-// navSlide();
-
-// var tween = gsap.to('.nav-links', {
-//     duration: 1, width:'50%', ease: 'power', reversed: true
-// })
-
-// var tween2 = gsap.to('.nav-links li', {
-//     duration: .5, delay: .5, opacity: 1, reversed: true
-// })
-
 const burger = document.querySelector('.burger');
 const nav = document.querySelector('.nav-links');
 const navLinks = document.querySelectorAll('.nav-links li')
 
-
-
-let width = screen.width
-
-
-
-window.addEventListener('resize', () => {
-    width = screen.width
-    console.log(width)
-    if (width >= 769) {
-        location.reload()
-    }
-})
+let width = document.body.offsetWidth
 
 let navClosed = true // used to toggle the animation
 
@@ -56,72 +10,33 @@ const burgerClick = () => {
     if (width <= 768) {
         navClosed = !navClosed 
         navClosed ? timeline.reverse() : timeline.play();
-        console.log(navClosed)
     } else {
         navClosed = true
         // navClosed = !navClosed
-        console.log(navClosed)
     }
     
 }
-
 
 burger.addEventListener('click', burgerClick)
 nav.addEventListener('click', burgerClick)
 
 const timeline = gsap.timeline(
-    { defaults: { duration: 1},
-    onReverseComplete: function(){ console.log('finish') }
+    { defaults: { duration: 1}
 })
 timeline
-    .to('.nav-links', {
-        x: 0, ease: 'power',  
-    })
-    .to('.nav-links li', {
-        opacity: 1,
-    }, '<.5').pause() // animation is paused by default
- 
-// const smootheScroll = (target,duration) => {
-//     var target = document.querySelector(target);
-//     var targetPosition = target.getBoundingClientRect().top;
-//     var startPosition = window.pageYOffset;
-//     var distance = targetPosition - startPosition;
-//     var startTime = null;
-
-//     function animation(currentTime){
-//         console.log(currentTime)
-//         if(startTime === null) startTime = currentTime;
-//         var timeElapsed = currentTime - startTime;
-//         var run = easeInOutQuad(timeElapsed,startPosition,distance,duration)
-//         window.scrollTo(0,run);
-//         if(timeElapsed < duration) requestAnimationFrame(animation);
-//     }
-
-//     function easeInOutQuad(t, b, c, d) {
-//         t /= d/2;
-//         if (t < 1) return c/2*t*t + b;
-//         t--;
-//         return -c/2 * (t*(t-2) - 1) + b;
-//     };
-
-//     requestAnimationFrame(animation);
-// }
-
-// var about_section = document.querySelector('.about_section');
-// var projects_section = document.querySelector('.projects_section');
-// var skills_section = document.querySelector('.skills_section');
-
-// about_section.addEventListener('click',function(){
-//     smootheScroll('#about_section', 1000);
-// });
+.to('.nav-links', {
+    x: 0, ease: 'power',  
+})
+.to('.nav-links li', {
+    opacity: 1,
+}, '<.5').pause() // animation is paused by default
 
 
-// projects_section.addEventListener('click',function(){
-//     smootheScroll('#projects_section', 1000);
-// });
-
-
-// skills_section.addEventListener('click',function(){
-//     smootheScroll('#skills_section', 1000);
-// });
+window.addEventListener('resize', () => {
+    width = document.body.offsetWidth
+    console.log(width)
+    if (width >= 769) {
+        location.reload()
+    }
+})
 
